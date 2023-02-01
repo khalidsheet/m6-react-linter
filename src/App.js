85d874ca5cb011f521a289a0c3a7c902e7550fea
Khalid Mohammad <khalid.m.sheet@gmail.com>
2023-02-01 12:45:31 +0300

    update prettier

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import "./App.css";
-import { useState } from "react";
-import { React } from "react";
+import './App.css';
+import { useState } from 'react';
+import { React } from 'react';
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
+     const [counter, setCounter] = useState(0);
 
-  return (
-    <div className="App">
-      <div>{counter}</div>
-      <button onClick={() => setCounter(counter + 1)}>Update</button>
-    </div>
-  );
+     return (
+          <div className="App">
+               <div>{counter}</div>
+               <button onClick={() => setCounter(counter + 1)}>Update</button>
+          </div>
+     );
 };
 
 export default App;
